db81b60a6dfb78d3b3dc6fd62b9472b0b1261736
JFSilvaM <joaofrsima2000@gmail.com>
2026-09-01 17:59:16 +0200

    first commit

---
+++ src/app/auth/register/index.tsx
@@ -0,0 +1,12 @@
+import { ThemedText } from "@/theme/components/themed-text";
+import { View } from "react-native";
+
+const RegisterScreen = () => {
+  return (
+    <View>
+      <ThemedText>RegisterScreen</ThemedText>
+    </View>
+  );
+};
+
+export default RegisterScreen;
