16b653a78c85a5215775fbd927e7399926653178
JFSilvaM <joaofrsima2000@gmail.com>
2026-09-02 17:33:38 +0200

    feat: add environment variables

--- src/core/auth/api/products-api.ts
+++ src/core/auth/api/products-api.ts
@@ -1,5 +1,17 @@
 import axios from "axios";
+import { Platform } from "react-native";
 
-const productsApi = axios.create({ baseURL: "localhost:3000/api" });
+const STAGE = process.env.EXPO_PUBLIC_STAGE || "dev";
+
+const API_URL =
+  STAGE === "prod"
+    ? process.env.EXPO_PUBLIC_API_URL
+    : Platform.OS === "ios"
+      ? process.env.EXPO_PUBLIC_API_URL_IOS
+      : process.env.EXPO_PUBLIC_API_URL_ANDROID;
+
+console.log({ STAGE, [Platform.OS]: API_URL });
+
+const productsApi = axios.create({ baseURL: API_URL });
 
 export { productsApi };
