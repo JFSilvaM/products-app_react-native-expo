10b7af3e4e9b0c4c9836adf920be3481078106a8
JFSilvaM <joaofrsima2000@gmail.com>
2026-09-02 12:43:03 +0200

    feat: add text font and colors

--- src/app/(products-app)/(home)/index.tsx
+++ src/app/(products-app)/(home)/index.tsx
@@ -1,10 +1,16 @@
 import { ThemedText } from "@/theme/components/themed-text";
+import { useTheme } from "@/theme/hooks/use-theme";
 import { View } from "react-native";
 
 const HomeScreen = () => {
+  const primary = useTheme().primary;
+
   return (
     <View>
-      <ThemedText>HomeScreen</ThemedText>
+      <ThemedText style={{ color: primary }}>HomeScreen</ThemedText>
+      <ThemedText font="regular">HomeScreen</ThemedText>
+      <ThemedText font="bold">HomeScreen</ThemedText>
+      <ThemedText font="thin">HomeScreen</ThemedText>
     </View>
   );
 };
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,28 @@
+import { useFonts } from "expo-font";
 import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import { useColorScheme } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
   const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    KanitRegular: require("@/assets/fonts/Kanit-Regular.ttf"),
+    KanitBold: require("@/assets/fonts/Kanit-Bold.ttf"),
+    KanitThin: require("@/assets/fonts/Kanit-Thin.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) SplashScreen.hideAsync();
+  }, [loaded]);
+
+  if (!loaded) return null;
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack />
+      <Stack screenOptions={{ headerShown: false }} />
     </ThemeProvider>
   );
 };
