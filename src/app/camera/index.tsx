933c10b3f37dbb755496add1912f4e388fd4289c
JFSilvaM <joaofrsima2000@gmail.com>
2026-09-15 12:14:49 +0200

    feat: add expo camera

--- src/app/camera/index.tsx
+++ src/app/camera/index.tsx
@@ -1,11 +1,79 @@
-import { Text, View } from "react-native";
+import { ThemedText } from "@/theme/components/themed-text";
+import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const CameraScreen = () => {
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) return <View />;
+
+  if (!permission.granted)
+    return (
+      <View
+        style={{
+          ...styles.container,
+          marginHorizontal: 30,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={styles.message}>
+          Necesitamos permiso para usar la cámara y la galería
+        </Text>
+
+        <TouchableOpacity onPress={requestPermission}>
+          <ThemedText type="subtitle">Solicitar permiso</ThemedText>
+        </TouchableOpacity>
+      </View>
+    );
+
+  const toggleCameraFacing = () =>
+    setFacing((current) => (current === "back" ? "front" : "back"));
+
   return (
-    <View>
-      <Text>CameraScreen</Text>
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing} />
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+          <Text style={styles.text}>Flip Camera</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default CameraScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 64,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    width: "100%",
+    paddingHorizontal: 64,
+  },
+  button: {
+    flex: 1,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
